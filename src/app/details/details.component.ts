00698ad46276aa55bbe6807a11c7a57c6dfda90e
Aleksandr Keizik <alex.keizik@gmail.com>
2025-04-11 16:18:03 +0300

    Add Input annotation for url params

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -7,7 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
-
-
-  
+  @Input() name: string = '';
 }
